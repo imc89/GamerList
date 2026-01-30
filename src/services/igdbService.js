8a64909118fbded3c6ab874a5d04017f43f3d43e
Iñigo Montánchez <mci.m89@gmail.com>
2026-01-30 14:05:59 +0100

    fix cors problem with corsproxy.io

--- src/services/igdbService.js
+++ src/services/igdbService.js
@@ -36,9 +36,9 @@ async function getAccessToken() {
         // But for the token endpoint, usually parameters are enough
         let url = `${AUTH_URL}?client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`;
 
-        // Use AllOrigins raw proxy in production
+        // Use ThingProxy in production
         if (!isDev) {
-            url = `https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`;
+            url = `https://thingproxy.freeboard.io/fetch/${url}`;
         }
 
         const response = await fetch(url, {
@@ -85,7 +85,7 @@ export async function searchGames(query) {
 
         let url = API_URL;
         if (!isDev) {
-            url = `https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`;
+            url = `https://thingproxy.freeboard.io/fetch/${url}`;
         }
 
         const response = await fetch(url, {
