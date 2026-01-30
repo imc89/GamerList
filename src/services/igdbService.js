e92fe1c3052ca70411a56f739cbf492ea8f00d19
Iñigo Montánchez <mci.m89@gmail.com>
2026-01-30 14:28:07 +0100

    fix cors problem with corsproxy.io

--- src/services/igdbService.js
+++ src/services/igdbService.js
@@ -36,9 +36,9 @@ async function getAccessToken() {
         // But for the token endpoint, usually parameters are enough
         let url = `${AUTH_URL}?client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`;
 
-        // Use corsproxy.io in production
+        // Use cors.eu.org in production
         if (!isDev) {
-            url = `https://corsproxy.io/?${encodeURIComponent(url)}`;
+            url = `https://cors.eu.org/${url}`;
         }
 
         const response = await fetch(url, {
@@ -85,7 +85,7 @@ export async function searchGames(query) {
 
         let url = API_URL;
         if (!isDev) {
-            url = `https://corsproxy.io/?${encodeURIComponent(url)}`;
+            url = `https://cors.eu.org/${url}`;
         }
 
         const response = await fetch(url, {
